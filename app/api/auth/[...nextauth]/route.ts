2cb31416232732943e7613938f43a711940b7a72
shefstack <133235174+shefstack@users.noreply.github.com>
2026-01-18 11:50:42 +0000

    debugging

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ export const runtime = "nodejs"
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
-
+console.log("VERCEL SECRET:", process.env.NEXTAUTH_SECRET)
 const handler = NextAuth({ ...authOptions, secret: process.env.NEXTAUTH_SECRET });
 export { handler as GET, handler as POST }
